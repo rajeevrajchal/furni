7d944951cc390e8e15878ca90753ee9eea5b458e
Rajeev Rajchal <rajeev@setsnail.com>
2024-09-12 21:50:04 +0200

    chore:initial

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,14 @@
+<nav>
+	<p>this is nav bar</p>
+</nav>
+
+<style lang="scss">
+	nav {
+		background-color: $primary-color;
+		color: white;
+		height: 4vh;
+		display: flex;
+		align-items: center;
+		@include pagePadding;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Navbar from '@comp/navbar.svelte';
+</script>
+
+<main id="home">
+	<Navbar />
+	<div class="container">
+		<h1>Page</h1>
+		<p>This is a page</p>
+	</div>
+</main>
+
+<style lang="scss">
+	main {
+		height: 100vh;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: start;
+		background: red;
+	}
+
+	.container {
+		background: 'red';
+		flex: 1;
+		@include pagePadding;
+	}
+</style>
